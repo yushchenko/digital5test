03fbe53951d5953dc0a76568a0a1591405d1689f
Valery Yushchenko <valery.yushchenko@gmail.com>
2011-07-24 09:01:33 +1000

    3 ways of fibs calculation are implemented, unit test fixed

--- src/fib.js
+++ src/fib.js
@@ -1,12 +1,47 @@
 
-function fibSimple(n) {
+// assume typeof n === 'number', n > 0 for all functions
 
+function fibSimple(n) { 
+
+    var fibs = [0, 1],
+        i = 2;
+
+    while (n > 1 && i <= n) {
+        fibs[i] = fibs[i-1] + fibs[i-2];
+        i += 1;
+    }
+
+    return fibs[n];
 }
 
-function fibMayBeBetter(n) {
-    
+function fibMayBeBetter(n) { // trying to use less memory, a bit of ugly
+
+    var fibMinus1 = 1, fibMinus2 = 0,
+        fib, i = 2;
+
+    if (n < 2) {
+        return n;
+    }
+
+    while (i <= n) {
+        fib = fibMinus1 + fibMinus2;
+        fibMinus2 = fibMinus1;
+        fibMinus1 = fib;
+        i += 1;
+    }
+
+    return fib;
 }
         
-function fibMemoization(n) {
-    
+function fibMemoization(n) { // avoiding the main recursion drawback
+
+    if (fibMemoization.fibs === undefined) {
+        fibMemoization.fibs = [0,1];
+    }
+
+    if (typeof fibMemoization.fibs[n] !== 'number') {
+        fibMemoization.fibs[n] = fibMemoization(n-1) + fibMemoization(n-2);
+    }
+
+    return fibMemoization.fibs[n];
 }
--- src/fib.spec.js
+++ src/fib.spec.js
@@ -3,14 +3,14 @@ describe("Fibonacci Sequence", function() {
   it("should calculate fibs in different ways", function() {
 
       var fibs = [0,1,1,2,3,5,8],
-          n, max = fibs.length + 1,
+          n, max = fibs.length,
 
           toTest = [fibSimple, fibMayBeBetter, fibMemoization],
           k, len = toTest.length;
 
       for (k = 0; k < len; k += 1) {
-          for (n = 1; n < max; n += 1) {
-              expect(toTest[k](n)).toEqual(fibs[n-1]);
+          for (n = 0; n < max; n += 1) {
+              expect(toTest[k](n)).toEqual(fibs[n]);
           }
       }
   });
